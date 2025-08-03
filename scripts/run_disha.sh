370cf733ace6abd1f05724ed1e381c7f552cb0b1
Aly Nasr <Aly.Nasr.L190@gmail.com>
2025-08-03 18:40:10 +0300

    split into 4

--- scripts/run_disha.sh
+++ scripts/run_disha.sh
@@ -6,8 +6,8 @@ n_layer=32
 n_embd=2560
 
 micro_bsz=16
-epoch_save=5
-epoch_steps=200
+epoch_save=1
+epoch_steps=300
 ctx_len=512
 
 disha_config='{"mode":"bat","load":"","r":64}'
@@ -19,7 +19,7 @@ python train.py --load_model $load_model \
 --n_layer $n_layer --n_embd $n_embd \
 --data_type binidx --dataload pad --loss_mask pad \
 --ctx_len $ctx_len --micro_bsz $micro_bsz \
---epoch_steps $epoch_steps --epoch_count 1 --epoch_begin 0 --epoch_save $epoch_save \
+--epoch_steps $epoch_steps --epoch_count 5 --epoch_begin 0 --epoch_save $epoch_save \
 --lr_init 2e-5 --lr_final 2e-5 --warmup_steps 0 --beta1 0.9 --beta2 0.99 --adam_eps 1e-8 \
 --accelerator gpu --devices 1 --precision bf16 --strategy deepspeed_stage_1 --grad_cp 1 \
 --my_testing "x070" \
