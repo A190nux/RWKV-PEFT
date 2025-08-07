6badb6fa6c02d23326519a56d980c1a870c64a5d
Aly Nasr <Aly.Nasr.L190@gmail.com>
2025-08-07 13:40:46 +0300

    part 3 with cosine annealing

--- scripts/merge_disha.sh
+++ scripts/merge_disha.sh
@@ -1,6 +1,6 @@
-base_model='/kaggle/input/rwkv-pth/rwkv7-g1-2.9b-20250519-ctx4096.pth'
-peft_checkpoint='/kaggle/input/rwkv-sft-tl-v1/rwkv-4.pth'
-output='/kaggle/working/rwkv-1-5.pth'
+base_model='/kaggle/input/rwkv-sft-tl-v1-2-5/rwkv-1-5.pth'
+peft_checkpoint='/kaggle/input/rwkv-sft-tl-v1-2-5/rwkv-4.pth'
+output='/kaggle/working/rwkv-2-5.pth'
 
 
 python merge/merge_disha.py --base_model $base_model \
--- scripts/run_disha.sh
+++ scripts/run_disha.sh
@@ -1,6 +1,6 @@
-load_model='/kaggle/working/rwkv-1-5.pth'
+load_model='/kaggle/working/rwkv-2-5.pth'
 proj_dir='/kaggle/working/'
-data_file='/kaggle/input/zh-en-post-dataset/split_part_2_text_document'
+data_file='/kaggle/input/zh-en-post-dataset/split_part_3_text_document'
 
 n_layer=32
 n_embd=2560
@@ -20,7 +20,7 @@ python train.py --load_model $load_model \
 --data_type binidx --dataload pad --loss_mask pad \
 --ctx_len $ctx_len --micro_bsz $micro_bsz \
 --epoch_steps $epoch_steps --epoch_count 5 --epoch_begin 0 --epoch_save $epoch_save \
---lr_init 2e-5 --lr_final 2e-5 --warmup_steps 0 --beta1 0.9 --beta2 0.99 --adam_eps 1e-8 \
+--lr_init 2e-5 --lr_final 1e-6 --warmup_steps 50 --beta1 0.9 --beta2 0.99 --adam_eps 1e-8 \
 --accelerator gpu --devices 1 --precision bf16 --strategy deepspeed_stage_1 --grad_cp 1 \
 --my_testing "x070" \
 --peft disha --disha_config $disha_config
