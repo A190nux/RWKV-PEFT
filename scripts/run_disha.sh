895e0e6d5cb5248b4bc4b314b4766a4a54c02921
Aly Nasr <Aly.Nasr.L190@gmail.com>
2025-08-14 21:12:58 +0300

    reduced power

--- scripts/run_disha.sh
+++ scripts/run_disha.sh
@@ -10,7 +10,7 @@ epoch_save=1
 epoch_steps=50
 ctx_len=18000
 
-disha_config='{"mode":"bat","load":"","r":64}'
+disha_config='{"mode":"bone","load":"","r":64}'
 
 
 python train.py --load_model $load_model \
@@ -21,6 +21,7 @@ python train.py --load_model $load_model \
 --ctx_len $ctx_len --micro_bsz $micro_bsz \
 --epoch_steps $epoch_steps --epoch_count 50 --epoch_begin 0 --epoch_save $epoch_save \
 --lr_init 2e-5 --lr_final 2e-5 --warmup_steps 50 --beta1 0.9 --beta2 0.99 --adam_eps 1e-8 \
---accelerator gpu --devices 2 --precision bf16 --strategy deepspeed_stage_1 --grad_cp 1 \
+--accelerator gpu --devices 2 --precision bf16 --strategy deepspeed_stage_2 --grad_cp 1 \
 --my_testing "x070" \
+--quant int8 \
 --peft disha --disha_config $disha_config
