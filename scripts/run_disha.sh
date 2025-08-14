aeb32b6caa27b37def0f839877020c7e94ba09ae
Aly Nasr <Aly.Nasr.L190@gmail.com>
2025-08-14 21:38:00 +0300

    nf4

--- scripts/run_disha.sh
+++ scripts/run_disha.sh
@@ -21,6 +21,7 @@ python train.py --load_model $load_model \
 --ctx_len $ctx_len --micro_bsz $micro_bsz \
 --epoch_steps $epoch_steps --epoch_count 50 --epoch_begin 0 --epoch_save $epoch_save \
 --lr_init 2e-5 --lr_final 2e-5 --warmup_steps 50 --beta1 0.9 --beta2 0.99 --adam_eps 1e-8 \
---accelerator gpu --devices 1 --precision bf16 --strategy deepspeed_stage_2 --grad_cp 1 \
+--accelerator gpu --devices 2 --precision bf16 --strategy deepspeed_stage_2 --grad_cp 1 \
 --my_testing "x070" \
+--quant nf4 \
 --peft disha --disha_config $disha_config
