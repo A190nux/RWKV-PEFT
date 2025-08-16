0a3bfb3002cb706de5f05d9088856696205a7e44
Aly Nasr <Aly.Nasr.L190@gmail.com>
2025-08-17 01:46:56 +0300

    stage 3

--- scripts/merge_disha.sh
+++ scripts/merge_disha.sh
@@ -1,6 +1,6 @@
-base_model='/kaggle/input/rwkv-sft-tl-v2-step1/rwkv-3b-stage0.pth'
-peft_checkpoint='/kaggle/input/rwkv-sft-tl-v2-step1/rwkv-34.pth'
-output='/kaggle/working/rwkv-3b-stage1.pth'
+base_model='/kaggle/input/rwkv-sft-tl-v2-step2/rwkv-3b-stage1.pth'
+peft_checkpoint='/kaggle/input/rwkv-sft-tl-v2-step2/rwkv-33.pth'
+output='/kaggle/working/rwkv-3b-stage2.pth'
 
 
 python merge/merge_disha.py --base_model $base_model \
